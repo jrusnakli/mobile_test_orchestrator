5038eacfc840d407b26803d29596815002326666
John Rusnak <john.j.rusnak@att.net>
2019-05-12 13:12:46 -0700

    Add working dir to yaml

--- .circleci/setup_emulator.sh
+++ .circleci/setup_emulator.sh
@@ -2,6 +2,6 @@ env
 echo "LS######"
 ls /usr/local/bin
 echo "########"
-echo Y | ${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "system-images;android-28;default;x86_64"
+yes | ${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "system-images;android-28;default;x86_64"
 ${ANDROID_SDK_ROOT}/tools/bin/avdmanager create avd -n test -k "system-images;android-28;default;x86_64" -d "pixel_xl"
 exit 0
