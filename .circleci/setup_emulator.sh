2fb4e0fc6aa7b59fbf2085f02a1ac36a641fa12c
John Rusnak <john.j.rusnak@att.net>
2019-05-12 23:30:43 -0700

    Create emulator proper as part of workflow

--- .circleci/setup_emulator.sh
+++
@@ -1,7 +0,0 @@
-env
-echo "LS######"
-ls /usr/local/bin
-echo "########"
-yes | ${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "system-images;android-28;default;x86_64"
-${ANDROID_SDK_ROOT}/tools/bin/avdmanager create avd -n test -k "system-images;android-28;default;x86_64" -d "pixel_xl"
-exit 0
