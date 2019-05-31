fdf1812d4d20a916e2bae72046f8e41613d674cf
john rusnak <jrusnak@linkedin.com>
2019-05-31 13:24:25 -0700

    Fixed test criteria from last update

--- orchestrator/test/test_device_log_class.py
+++ orchestrator/test/test_device_log_class.py
@@ -20,7 +20,7 @@ class TestDeviceLog:
         log.set_logcat_buffer_size("20M")
         assert log.logcat_buffer_size == '20Mb'
         log.set_logcat_buffer_size(DeviceLog.DEFAULT_LOGCAT_BUFFER_SIZE)
-        assert log.logcat_buffer_size == '1Mb'
+        assert log.logcat_buffer_size == '5Mb'
 
     def test_logcat_and_clear(self, device: Device, test_butler_service):
         uninstall_apk(test_butler_service, device)
