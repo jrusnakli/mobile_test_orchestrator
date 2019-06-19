cf4ee9de99db547138834ea056397bb21e4e5aec
Alisa Yamanaka <ayamanaka@linkedin.com>
2019-06-19 16:53:39 -0700

    Add fixture for setting up apps (#31)

--- orchestrator/test/test_application_class.py
+++ orchestrator/test/test_application_class.py
@@ -12,12 +12,12 @@ from unittest.mock import patch, PropertyMock
 import pytest
 
 from androidtestorchestrator import Device
-from androidtestorchestrator.application import Application
+from androidtestorchestrator.application import Application, TestApplication
 from .support import uninstall_apk
 
 
 # noinspection PyShadowingNames
-class TestApplication:
+class TestApplicationClass:
 
     def test_install_uninstall(self, device: Device, support_app: str):
         uninstall_apk(support_app, device)
@@ -33,29 +33,25 @@ class TestApplication:
             app.uninstall()
             assert app.package_name not in device.list_installed_packages()
 
-    def test_grant_permissions(self, device: Device, support_test_app: str):
-        uninstall_apk(support_test_app, device)
-        app = Application.from_apk(support_test_app, device)
-        assert app.package_name.endswith(".test")
-        try:
-            permission = "android.permission.WRITE_EXTERNAL_STORAGE"
-            app.grant_permissions([permission])
-            output = device.execute_remote_cmd("shell", "dumpsys", "package", app.package_name, capture_stdout=True,
-                                               timeout=10)
-            perms = []
-            look_for_perms = False
-            for line in output.splitlines():
-                if "granted=true" in line:
-                    perms.append(line.strip().split(':', 1)[0])
-                if "grantedPermissions" in line:
-                    # older reporting style .  Ugh.  Yeah for inconsistencies
-                    look_for_perms = True
-                if look_for_perms:
-                    if "permission" in line:
-                        perms.append(line.strip())
-            assert permission in perms
-        finally:
-            app.uninstall()
+    def test_grant_permissions(self, device: Device, install_app, support_test_app):
+        test_app = install_app(TestApplication, support_test_app)
+        assert test_app.package_name.endswith(".test")
+        permission = "android.permission.WRITE_EXTERNAL_STORAGE"
+        test_app.grant_permissions([permission])
+        output = device.execute_remote_cmd("shell", "dumpsys", "package", test_app.package_name, capture_stdout=True,
+                                           timeout=10)
+        perms = []
+        look_for_perms = False
+        for line in output.splitlines():
+            if "granted=true" in line:
+                perms.append(line.strip().split(':', 1)[0])
+            if "grantedPermissions" in line:
+                # older reporting style .  Ugh.  Yeah for inconsistencies
+                look_for_perms = True
+            if look_for_perms:
+                if "permission" in line:
+                    perms.append(line.strip())
+        assert permission in perms
 
     # noinspection PyBroadException
     @staticmethod
@@ -88,40 +84,28 @@ class TestApplication:
             except:
                 return False
 
-    def test_start_stop(self, device: Device, support_app: str):  # noqa
-        uninstall_apk(support_app, device)
-        app = Application.from_apk(support_app, device)
-        try:
-            app.start(".MainActivity")
-            time.sleep(3)  # Have to give time to "come up" :-(
-            assert self.pidof(app), "No pid found for app; app not started as expected"
-            app.stop(force=True)
-            if self.pidof(app):
-                time.sleep(3)  # allow slow emulators to catch up
-            pidoutput = app.device.execute_remote_cmd("shell", "pidof", "-s", app.package_name, fail_on_error_code=lambda x: x < 0)
-            assert not self.pidof(app), f"pidof indicated app is not stopped as expected; output of pidof is: {pidoutput}"
-        finally:
-            app.uninstall()
+    def test_start_stop(self, install_app, support_app: str):  # noqa
+        app = install_app(Application, support_app)
+        app.start(".MainActivity")
+        time.sleep(3)  # Have to give time to "come up" :-(
+        assert self.pidof(app), "No pid found for app; app not started as expected"
+        app.stop(force=True)
+        if self.pidof(app):
+            time.sleep(3)  # allow slow emulators to catch up
+        pidoutput = app.device.execute_remote_cmd("shell", "pidof", "-s", app.package_name, fail_on_error_code=lambda x: x < 0)
+        assert not self.pidof(app), f"pidof indicated app is not stopped as expected; output of pidof is: {pidoutput}"
 
-    def test_monkey(self, device: Device, support_app: str):  # noqa
-        uninstall_apk(support_app, device)
+    def test_monkey(self, device: Device, support_app):  # noqa
         app = asyncio.get_event_loop().run_until_complete(Application.from_apk_async(support_app, device))
-        try:
-            app.monkey()
-            time.sleep(3)
-            assert self.pidof(app), "Failed to start app"
-            app.stop(force=True)
-            assert not self.pidof(app), "Failed to stop app"
-        finally:
-            app.uninstall()
+        app.monkey()
+        time.sleep(3)
+        assert self.pidof(app), "Failed to start app"
+        app.stop(force=True)
+        assert not self.pidof(app), "Failed to stop app"
 
-    def test_clear_data(self, device: Device, support_app: str):  # noqa
-        uninstall_apk(support_app, device)
-        app = Application.from_apk(support_app, device)
-        try:
-            app.clear_data()  # should not raise exception
-        finally:
-            app.uninstall()
+    def test_clear_data(self, install_app, support_app: str):  # noqa
+        app = install_app(Application, support_app)
+        app.clear_data()  # should not raise exception
 
     def test_version_invalid_package(self, device: Device):
         with pytest.raises(Exception):
@@ -133,52 +117,43 @@ class TestApplication:
             app.uninstall()
             assert mock_logger.error.called
 
-    def test_clean_kill_throws_exception_when_home_screen_not_active(self, device: Device, support_app: str):
-        app = Application.from_apk(support_app, device)
-        try:
-            with patch('androidtestorchestrator.Device.home_screen_active', new_callable=PropertyMock) as mock_home_screen_active:
-                # Force home_screen_active to be false to indicate clean_kill failed
-                mock_home_screen_active.return_value = False
+    def test_clean_kill_throws_exception_when_home_screen_not_active(self, install_app, device: Device, support_app: str):
+        app = install_app(Application, support_app)
+        with patch('androidtestorchestrator.Device.home_screen_active', new_callable=PropertyMock) as mock_home_screen_active:
+            # Force home_screen_active to be false to indicate clean_kill failed
+            mock_home_screen_active.return_value = False
+            app.start(".MainActivity")
+            time.sleep(3)   # Give app time to come up
+            assert device.foreground_activity() == app.package_name
+            with pytest.raises(Exception) as exc_info:
+                app.clean_kill()
+            assert "Failed to background current foreground app" in str(exc_info)
+
+    def test_clean_kill_throws_exception_when_pid_still_existing(self, install_app, device: Device, support_app: str):
+        app = install_app(Application, support_app)
+        with patch('androidtestorchestrator.Device.home_screen_active', new_callable=PropertyMock) as mock_home_screen_active:
+            with patch('androidtestorchestrator.application.Application.pid', new_callable=PropertyMock) as mock_pid:
+                # Force home_screen_active to be True to indicate clean_kill made it to the home screen
+                mock_home_screen_active.return_value = True
+                # Force pid to return a fake process id to indicate clean_kill failed
+                mock_pid.return_value = 1234
                 app.start(".MainActivity")
-                time.sleep(3)   # Give app time to come up
+                time.sleep(3)  # Give app time to come up
                 assert device.foreground_activity() == app.package_name
                 with pytest.raises(Exception) as exc_info:
                     app.clean_kill()
-                assert "Failed to background current foreground app" in str(exc_info)
-        finally:
-            app.uninstall()
+                assert "Detected app process is still running" in str(exc_info)
 
-    def test_clean_kill_throws_exception_when_pid_still_existing(self, device: Device, support_app: str):
-        app = Application.from_apk(support_app, device)
-        try:
-            with patch('androidtestorchestrator.Device.home_screen_active', new_callable=PropertyMock) as mock_home_screen_active:
-                with patch('androidtestorchestrator.application.Application.pid', new_callable=PropertyMock) as mock_pid:
-                    # Force home_screen_active to be True to indicate clean_kill made it to the home screen
-                    mock_home_screen_active.return_value = True
-                    # Force pid to return a fake process id to indicate clean_kill failed
-                    mock_pid.return_value = 1234
-                    app.start(".MainActivity")
-                    time.sleep(3)  # Give app time to come up
-                    assert device.foreground_activity() == app.package_name
-                    with pytest.raises(Exception) as exc_info:
-                        app.clean_kill()
-                    assert "Detected app process is still running" in str(exc_info)
-        finally:
-            app.uninstall()
-
-    def test_clean_kill_succeeds(self, device: Device, support_app: str):
-        app = Application.from_apk(support_app, device)
-        try:
-            with patch('androidtestorchestrator.Device.home_screen_active', new_callable=PropertyMock) as mock_home_screen_active:
-                with patch('androidtestorchestrator.application.Application.pid', new_callable=PropertyMock) as mock_pid:
-                    # Force home_screen_active to be True to indicate clean_kill made it to the home screen
-                    mock_home_screen_active.return_value = True
-                    # Force pid to return None to make it seem like the process was actually killed
-                    mock_pid.return_value = None
-                    app.start(".MainActivity")
-                    time.sleep(3)  # Give app time to come up
-                    assert device.foreground_activity() == app.package_name
-                    # clean_kill doesn't return anything, so just make sure no exception is raised
-                    app.clean_kill()
-        finally:
-            app.uninstall()
+    def test_clean_kill_succeeds(self, install_app, device: Device, support_app: str):
+        app = install_app(Application, support_app)
+        with patch('androidtestorchestrator.Device.home_screen_active', new_callable=PropertyMock) as mock_home_screen_active:
+            with patch('androidtestorchestrator.application.Application.pid', new_callable=PropertyMock) as mock_pid:
+                # Force home_screen_active to be True to indicate clean_kill made it to the home screen
+                mock_home_screen_active.return_value = True
+                # Force pid to return None to make it seem like the process was actually killed
+                mock_pid.return_value = None
+                app.start(".MainActivity")
+                time.sleep(3)  # Give app time to come up
+                assert device.foreground_activity() == app.package_name
+                # clean_kill doesn't return anything, so just make sure no exception is raised
+                app.clean_kill()
