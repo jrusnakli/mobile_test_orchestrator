b44e0fc51c07a318a48b9a398b1c123b864ce28c
Graham Turbyne <gturbyne@linkedin.com>
2019-07-12 14:53:09 -0700

    Adding TestRunResult, TestId, and TestResult classes as near-copies from mdl-integration. Also adding in test_started to TestListener

--- orchestrator/src/androidtestorchestrator/reporting.py
+++ orchestrator/src/androidtestorchestrator/reporting.py
@@ -1,5 +1,8 @@
+import datetime
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from enum import Enum
+from typing import Any, Dict, Optional
 
 
 class TestStatus(Enum):
@@ -7,6 +10,7 @@ class TestStatus(Enum):
     FAILED = "FAILED"
     SKIPPED = "SKIPPED"
     ASSUMPTION_VIOLATED = "ASSUMPTION_VIOLATED"
+    INCOMPLETE = "INCOMPLETE"
 
     def __repr__(self) -> str:
         return self.value  # type: ignore
@@ -14,7 +18,7 @@ class TestStatus(Enum):
 
 class TestListener(ABC):
     """
-    Absrtaction for reporting test status (coming from InstrumentationOutputParser)
+    Abstraction for reporting test status (coming from InstrumentationOutputParser)
 
     Clients implement this to receive live test status as they are executed.
     """
@@ -79,6 +83,16 @@ class TestListener(ABC):
         """
 
     @abstractmethod
+    def test_started(self, test_name: str, test_class: str, test_no: int, msg: str = "") -> None:
+        """
+        signal test has started
+        :param test_name: name of test that has started
+        :param test_class: class name of test
+        :param test_no: test # in sequence of execution
+        :param msg: general message if any, else empty
+        """
+
+    @abstractmethod
     def test_ended(self, test_name: str, test_class: str, test_no: int, duration: float, msg: str = "") -> None:
         """
         signal test has ended, presumably with success
@@ -88,3 +102,140 @@ class TestListener(ABC):
         :param duration: time to execute the test
         :param msg: general message is any, else empty
         """
+
+
+class TestResult(object):
+    """
+    Result of an individual test run.
+    """
+
+    def __init__(self) -> None:
+        self.status: TestStatus = TestStatus.INCOMPLETE
+        self.start_time: datetime = datetime.datetime.utcnow()
+        self.end_time: Optional[datetime] = None
+        self.stack_trace: Optional[str] = None
+        self.data: Dict[str, Any] = {}
+
+    @property
+    def duration(self) -> datetime.timedelta:
+        return (self.end_time - self.start_time).total_seconds()
+
+    def failed(self, stack_trace: str) -> None:
+        """
+        Marks this test as failed
+        :param stack_trace: A stack trace for the failure
+        """
+        self.status = TestStatus.FAILURE
+        self.stack_trace = stack_trace
+
+    def assumption_failure(self, stack_trace: str) -> None:
+        """
+        Marks this test as an assumption failure
+        :param stack_trace: A stack trace for the assumption violation
+        """
+        self.status = TestStatus.ASSUMPTION_FAILURE
+        self.stack_trace = stack_trace
+
+    def ignored(self) -> None:
+        """
+        Marks this test as ignored (skipped)
+        """
+        self.status = TestStatus.IGNORED
+
+    def ended(self) -> None:
+        """
+        Marks the end of the test. If not failed or ignored, test is marked as passed.
+        """
+        if self.status == TestStatus.INCOMPLETE:
+            self.status = TestStatus.PASSED
+        self.end_time = datetime.datetime.utcnow()
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__ + str(self.__dict__)
+
+
+class TestRunResult(TestListener):
+    """
+    Result of a whole test run.
+
+    Base implementation of TestListener that collects results into a dictionary, and extracts need for timing and
+    result collection operations away from test methods.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.test_suite_name = "not started"
+        self.duration = 0
+        self.start_time = None
+        self.end_time = None
+        self.error_message = None
+        self.test_results = Dict[TestId, TestResult] = {}
+
+    @property
+    def is_complete(self) -> bool:
+        """:return: True iff test_suite_ended has been called"""
+        return self.end_time is not None
+
+    @property
+    def is_failed(self) -> bool:
+        """:return: True iff test_suite_failed has been called"""
+        return self.error_message is not None
+
+    def test_count(self, status: Optional[TestStatus] = None) -> int:
+        """
+        :param status: A TestResult status (PASSED, FAILED, etc). If not specified, returns the total number of tests.
+        :return: the number of tests with the given status
+        """
+        if status is None:
+            return len(self.test_results)
+        return sum(1 for result in self.test_results.values() if result.status == status)
+
+    def test_suite_started(self, test_suite_name: str) -> None:
+        self.test_suite_name = test_suite_name
+        self.duration = 0
+        self.start_time = datetime.datetime.utcnow()
+        self.end_time = None
+        self.error_message = None
+
+    def test_suite_ended(self, test_suite_name: str, test_count: int, execution_time: float = None) -> None:
+        self.end_time = datetime.datetime.utcnow()
+        self.duration += execution_time if execution_time is not None \
+            else (self.end_time - self.start_time).total_seconds()
+
+    def test_suite_errored(self, test_suite_name: str, status_code: int, exc_message: str = "") -> None:
+        self.test_suite_name = test_suite_name
+        self.error_message = exc_message
+
+    def test_failed(self, test_name: str, test_class: str, test_no: int, stack: str, msg: str = "") -> None:
+        self._get_test_result(test_class, test_name).failed(stack)
+
+    def test_ignored(self, test_name: str, test_class: str, test_no: int, msg: str = "") -> None:
+        self._get_test_result(test_class, test_name).ignored()
+
+    def test_assumption_violated(self, test_name: str, test_class: str, test_no: int, reason: str) -> None:
+        self._get_test_result(test_class, test_name).assumption_failure(reason)
+
+    def test_started(self, test_name: str, test_class: str, test_no: int, msg: str = "") -> None:
+        self.test_results[TestId(test_class, test_name)] = TestResult()
+
+    def test_ended(self, test_name: str, test_class: str, test_no: int, duration: float, msg: str = "") -> None:
+        result = self.test_results.setdefault(TestId(test_class, test_name), TestResult())
+        result.ended()
+
+    def _get_test_result(self, class_name: str, test_name: str) -> TestResult:
+        test_id = TestId(class_name, test_name)
+        result = self.test_results.get(test_id, None)
+        if result is None:
+            # TODO: Should we add any output here?
+            result = TestResult()
+            self.test_results[test_id] = result
+        return result
+
+
+@dataclass(frozen=True)
+class TestId(object):
+    """
+    A test identifier. Used as a key for test results.
+    """
+    class_name: Optional[str]
+    test_name: str
