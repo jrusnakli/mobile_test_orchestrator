7bd6d412380f96846a86b20094fe63abedf5e3d4
john rusnak <jrusnak@linkedin.com>
2020-01-21 13:42:15 -0800

    Updated comments

--- orchestrator/src/androidtestorchestrator/testing.py
+++ orchestrator/src/androidtestorchestrator/testing.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import time
+from contextlib import suppress
 
 from typing import Dict, List, Optional, Type
 from types import TracebackType
@@ -13,14 +14,23 @@ log = logging.getLogger()
 class EspressoTestPreparation:
     """
     Class used to prepare a device for test execution, including installing app, configuring settings/properties, etc.
+
+    Typically used as a context manager that will then automatically call cleanup() at exit.  The class provides
+    a list of features to setup and configure a device before test execution and teardown afterwards.
+    This includes:
+    * Installation of a app under test and test app to testit
+    * Ability to grant all user permissions (to prevent unwanted pop-ups) upon install
+    * Ability to configure settings and system properties of the device (restored to original values on exit)
+    * Ability to upload test vectors to external storage
+    * Ability to verify network connection to a resource
     """
 
     def __init__(self, device: Device, path_to_apk: str, path_to_test_apk: str, grant_all_user_permissions: bool = True):
         """
 
         :param device:  device to install and run test app on
-        :param path_to_apk: Path to apk bundle for target app under test
-        :param path_to_test_apk: Path to apk for test app
+        :param path_to_apk: Path to apk bundle for target app
+        :param path_to_test_apk: Path to apk bundle for test app
         :param grant_all_user_permissions: If True, grant all user permissions defined in the manifest of the app and
           test app (prevents pop-ups from occurring on first request for a user permission that can interfere
           with tests)
@@ -33,6 +43,8 @@ class EspressoTestPreparation:
         self._data_files: List[str] = []
         if grant_all_user_permissions:
             self._test_app.grant_permissions()
+        self._restoration_settings = {}
+        self._restoration_properties = {}
 
     @property
     def test_app(self) -> TestApplication:
@@ -43,10 +55,10 @@ class EspressoTestPreparation:
         if settings:
             for setting, value in settings.items():
                 ns, key = setting.split(':')
-                self._device.set_device_setting(ns, key, value)
+                self._restoration_settings[(ns, key)] = self._device.set_device_setting(ns, key, value)
         if properties:
             for property, value in properties.items():
-                self._device.set_system_property(property, value)
+                self._restoration_properties[property] = self._device.set_system_property(property, value)
 
     def setup_device(self, paths_to_foreign_apks: List[str]) -> None:
         for path in paths_to_foreign_apks:
@@ -54,7 +66,7 @@ class EspressoTestPreparation:
 
     def upload_test_vectors(self, root_path: str) -> float:
         """
-        upload test vectors to external storage on device for use by tests
+        Upload test vectors to external storage on device for use by tests
         :param root_path: local path that is the root where data files reside;  directory structure will be mimiced below
             this level
         :return: time in milliseconds it took to complete
@@ -74,9 +86,10 @@ class EspressoTestPreparation:
 
     def verify_network_connection(self, domain: str, count: int = 3) -> None:
         """
+        Verify connection to given domain is active.
         :param domain: address to test connection to
         :param count: number of packets to test
-        :return: number of failed packets (aka 0 on success)
+        :raises: IOError on failure to successfully ping given number of packets
         """
         lost_packet_count = self._device.check_network_connection(domain, count)
         if lost_packet_count > 0:
@@ -96,6 +109,12 @@ class EspressoTestPreparation:
                 self._storage.remove(remote_path)
             except Exception:
                 log.error(f"Failed to remove remote file {remote_path} from device {self._device.device_id}")
+        for ns,key in self._restoration_settings:
+            with suppress(Exception):
+                self._device.set_device_setting(ns, key, self._restoration_settings[(ns,key)] or '\"\"')
+        for prop in self._restoration_properties:
+            with suppress(Exception):
+                self._device.set_system_property(prop, self._restoration_properties[prop] or '\"\"')
 
     def __enter__(self) -> "EspressoTestPreparation":
         return self
