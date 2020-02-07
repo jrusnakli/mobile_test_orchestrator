e3f6689d1bb9fea761a1954c5cd8dc656f229660
john rusnak <jrusnak@linkedin.com>
2020-02-07 14:45:24 -0800

    updated version to 1.1.1

--- orchestrator/setup.py
+++ orchestrator/setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='androidtestorchestrator',
-    version='1.1.0',
+    version='1.1.1',
     package_dir={'': 'src'},
     packages=setuptools.find_packages('src'),
     include_package_data=True,
