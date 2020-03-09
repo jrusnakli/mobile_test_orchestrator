9b976c31c8857604e201b88aad2b9cabc702b150
john rusnak <jrusnak@linkedin.com>
2020-03-09 13:56:59 -0700

    Bump version

--- orchestrator/setup.py
+++ orchestrator/setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='androidtestorchestrator',
-    version='1.1.4',
+    version='1.2.0',
     package_dir={'': 'src'},
     packages=setuptools.find_packages('src'),
     include_package_data=True,
