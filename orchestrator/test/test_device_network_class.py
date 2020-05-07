ea4764bf2df06db929038aaeca8ef9beff4f8679
jrusnakli <jrusnak@linkedin.com>
2020-05-07 08:51:43 -0700

    Added port forward/reverse forward tests

--- orchestrator/test/test_device_network_class.py
+++ orchestrator/test/test_device_network_class.py
@@ -5,3 +5,23 @@ class TestDeviceNetwork:
 
     def test_check_network_connect(self, device: Device):
         assert DeviceNetwork(device).check_network_connection("localhost", count=3) == 0
+
+    def test_port_forward(self, device: Device):
+        device_network = DeviceNetwork(device)
+        device_network.port_forward(32451, 29323)
+        output = device.execute_remote_cmd("forward", "--list")
+        assert "32451" in output
+        device_network.remove_port_forward(29323)
+        output = device.execute_remote_cmd("forward", "--list")
+        assert "32451" not in output
+        assert "29323" not in output
+
+    def test_reverse_port_forward(self, device: Device):
+        device_network = DeviceNetwork(device)
+        device_network.reverse_port_forward(32451, 29323)
+        output = device.execute_remote_cmd("reverse", "--list")
+        assert "29323" in output
+        device_network.remove_reverse_port_forward(32451)
+        output = device.execute_remote_cmd("reverse", "--list")
+        assert "29323" not in output
+        assert "32451" not in output
